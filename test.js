3f77de86e0db189e2ead37ea7bb5cc6819687bb1
quzzQueth <hhhhyetr456@gmail.com>
2024-02-29 21:49:42 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+const menuDiv = document.querySelector('.top_menu_div');
+menuDiv.style.backgroundImage = 'linear-gradient(to right, #3096F8, #2D4AC5)';
+menuDiv.style.borderRadius = '8px';
+menuDiv.style.padding = '10px';
+menuDiv.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+menuDiv.style.color = '#fff';
