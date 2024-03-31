20c56d2e4ff7041f23c6a66a7face1f3acbe4c45
quzzQueth <hhhhyetr456@gmail.com>
2024-03-31 18:36:38 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 const menuDiv = document.querySelector('.top_menu_div');
-menuDiv.style.backgroundImage = 'linear-gradient(to right, #3096F8, #2D4AC5)';
-menuDiv.style.borderRadius = '8px';
-menuDiv.style.padding = '10px';
-menuDiv.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
-menuDiv.style.color = '#fff';
-menuDiv.style.background = 'linear-gradient(to right, red, orange)';
-menuDiv.style.WebkitBackgroundClip = 'text';
-menuDiv.style.WebkitTextFillColor = 'transparent';
+
+// Создайте стиль, который будет добавлен в документ
+const style = document.createElement('style');
+style.textContent =
+  `
+  @keyframes blink {
+    0% {color: lightgreen; background-color: #2D4AC5;}
+    50% {color: black; background-color: #3096F8;}
+    100% {color: lightgreen; background-color: #2D4AC5;}
+  }
+  .top_menu_div {
+    animation: blink 1s linear infinite;
+    background-image: linear-gradient(to right, #3096F8, #2D4AC5);
+    border-radius: 8px;
+    padding: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    color: #fff;
+  }
+  `;
+
+// Добавьте созданный стиль в документ
+document.head.appendChild(style);
